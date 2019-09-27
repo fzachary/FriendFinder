ab57a7c3c34a4021556832400c65b86bcf577ff2
Frank Zachary <fzachary@gmail.com>
2019-09-26 23:58:29 -0400

    files, html/JS for htmls, started the API data

---
+++ app/data/friends.js
@@ -0,0 +1,38 @@
+import { builtinModules } from "module";
+
+var friends = [
+    {
+        name: "Ahmed",
+        photo: "https://media.licdn.com/mpr/mpr/shrinknp_400_400/p/6/005/1bd/3435aa3.jpg",
+        scores: [
+            5,
+            1,
+            4,
+            4,
+            5,
+            1,
+            2,
+            5,
+            4,
+            1
+        ]
+    },
+    {
+        name: "Demha",
+        photo: "_____",
+        scores: [
+            1,
+            5,
+            2,
+            2,
+            1,
+            5,
+            4,
+            1,
+            2,
+            5
+        ]
+    }
+];
+
+module.exports = friends;
